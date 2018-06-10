c5d78185c717624c2490b7e9ebbe564a0d645a43
DhanasekaranMani <Dhana.Mani@hotmail.co.uk>
2018-06-10 20:04:34 +0100

    TDD Sample Project

---
+++ FizzBuzz.Library/FizzBuzzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int input)
+        {
+            //if (input % 3 == 0 && input % 5 == 0)
+            //{
+            //    return "FizzBuzz";
+            //}
+            //if (input%3 == 0)
+            //{
+            //    return "Fizz";
+            //}
+            //if (input%5 == 0)
+            //{
+            //    return "Buzz";
+            //}
+            //return input.ToString();
+
+            string output = string.Empty;
+            if (input % 3 == 0)
+                output += "Fizz";
+            if (input % 5 == 0)
+                output += "Buzz";
+            if (output == string.Empty)
+                output += input.ToString();
+
+            return output;
+
+        }
+    }
+}
